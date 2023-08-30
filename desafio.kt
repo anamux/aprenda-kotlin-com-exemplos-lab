f5dd830371ab6494548e095f875eb518ab9f0fe8
Ana Lídia Muxfeldt <50466947+anamux@users.noreply.github.com>
2023-08-30 15:55:54 -0300

    Alteração da class Usuario para data class; Inclusão das classes GerenciadorUsuario e GerenciadorConteudos, para incluir novos usuários e conteúdos na mutableList, na data class Formação foi criada uma fun mostarInscritos, que imprime na tela a lista dos alunos inscritos na formação, além disso, houveram as alterações na fun main testando as classes e as funções adicionadas.

--- desafio.kt
+++ desafio.kt
@@ -1,21 +1,67 @@
-// [Template no Kotlin Playground](https://pl.kotl.in/WcteahpyN)
-
 enum class Nivel { BASICO, INTERMEDIARIO, DIFICIL }
 
-class Usuario
+data class Usuario (val nome: String)
 
-data class ConteudoEducacional(var nome: String, val duracao: Int = 60)
+class GerenciadorUsuarios{
+    val usuario: MutableList<Usuario> = mutableListOf(Usuario("Aluno1"), Usuario("Aluno2"),
+        Usuario("Aluno3"))
 
-data class Formacao(val nome: String, var conteudos: List<ConteudoEducacional>) {
+    fun addNovoUsuario (novoUsuario: Usuario){
+        usuario.add(novoUsuario)
+    }
+}
+
+data class ConteudoEducacional(val nomeConteudoEducacional: String, val duracao: Int = 60, val nivel: Nivel)
+
+class GerenciadorConteudos {
+    companion object {
+        val conteudoEducacional: MutableList<ConteudoEducacional> =
+            mutableListOf(
+                ConteudoEducacional("Versionamento de Código", 120, Nivel.BASICO),
+                ConteudoEducacional("Linguagem Kotlin", 180, Nivel.INTERMEDIARIO),
+                ConteudoEducacional("Desenvolvimento Mobile", 240, Nivel.DIFICIL)
+            )
+    }
+
+    fun addNovoConteudo (novoConteudo: ConteudoEducacional){
+        conteudoEducacional.add(novoConteudo)
+    }
+
+}
+
+data class Formacao(val nomeFormacao: String, val gerenciadorUsuarios: GerenciadorUsuarios, val gerenciadorconteudos: GerenciadorConteudos) {
 
     val inscritos = mutableListOf<Usuario>()
-    
-    fun matricular(usuario: Usuario) {
-        TODO("Utilize o parâmetro $usuario para simular uma matrícula (usar a lista de $inscritos).")
+
+    fun matricular(vararg usuarios: Usuario) {
+        inscritos.addAll(usuarios)
+    }
+    fun mostrarInscritos() {
+        println("Inscritos na formação $nomeFormacao:")
+        inscritos.forEach { println(it.nome) }
     }
 }
 
 fun main() {
-    TODO("Analise as classes modeladas para este domínio de aplicação e pense em formas de evoluí-las.")
-    TODO("Simule alguns cenários de teste. Para isso, crie alguns objetos usando as classes em questão.")
+
+    val gerenciadorUsuarios = GerenciadorUsuarios()
+    val gerenciadorConteudos = GerenciadorConteudos()
+
+    gerenciadorUsuarios.addNovoUsuario(Usuario("Aluno4"))
+
+    val novoConteudo = ConteudoEducacional("Novo Conteudo", 60, Nivel.BASICO)
+    gerenciadorConteudos.addNovoConteudo(novoConteudo)
+    
+   
+    println("Usuarios: ")
+    gerenciadorUsuarios.usuario.forEach { println(it.nome)}
+
+    println("Conteúdos:")
+    GerenciadorConteudos.conteudoEducacional.forEach {
+        println("Nome: ${it.nomeConteudoEducacional}, Duração: ${it.duracao}, Nível: ${it.nivel}")
+    }
+    val formacao = Formacao("Curso de Programação", gerenciadorUsuarios, gerenciadorConteudos)
+    formacao.matricular(Usuario("Aluno4"))
+    formacao.matricular(Usuario("Aluno1"))
+    formacao.mostrarInscritos()
 }
